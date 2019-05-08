e07da10a31c505bf5bb4cddead940ae8232d0acb
miduo Yu <2225122776@qq.com>
2019-05-08 07:38:45 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+
+Route::resource('users', 'UsersController');
