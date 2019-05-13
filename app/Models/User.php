bffced14fc5de6aaf9f6fcdcd6bbf935fb54c2bd
miduo Yu <2225122776@qq.com>
2019-05-13 02:00:36 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,8 +62,10 @@ class User extends Authenticatable
     }
 
     public function feed()
-    {
-        return $this->statuses()->orderBy('created_at','desc');
+    {   $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        // return $this->statuses()->orderBy('created_at','desc');
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
     }
     //多对多关联
     //获取粉丝关系列表
